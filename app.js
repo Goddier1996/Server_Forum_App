a0f0b596046b1b37ed5465957e6c357f3af48511
artium_96 <artium20@gmail.com>
2024-11-16 11:21:42 +0200

    update server

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require(`express`)
 const cors = require(`cors`)
 const http = require(`http`)
 
-
 let app = express()
 app.use(cors())
 app.use(express.json())
-
 
 app.use('/api/messages', require('./routes/messages'))
 app.use('/api/category', require('./routes/category'))
@@ -16,7 +14,6 @@ app.use('/api/users', require('./routes/users'))
 
 const PORT = process.env.PORT || 5000
 process.setMaxListeners(100)
-
 
 const server = http.createServer(app)
 server.listen(PORT, () => { console.log(`the server is live at http://localhost:${PORT}`) })
